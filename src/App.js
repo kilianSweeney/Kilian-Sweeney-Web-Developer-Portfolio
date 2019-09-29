b7fe2ff9cfdc8b859b0d1e2dbebb3e2e82f00f35
Kilian Sweeney <kilian_sweeney@yahoo.com>
2019-09-29 11:41:33 -0500

    no message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {work} from './constants/constants'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="p-4">
+        <span>Kilian Sweeney: Developer | Designer</span>
       </header>
-    </div>
+      <section>
+        <div className="col-md-1 text-center">
+          <div className="list-group list-group-horizontal list-group-flush">
+            <a className="list-group-item list-group-item-action btn btn-social btn-linkdin" href="https://github.com/kilianSweeney"><FontAwesomeIcon icon={faGithub} /></a>
+            <a className="list-group-item list-group-item-action btn btn-social btn-linkdin" href="https://www.linkedin.com/in/kilian-sweeney-273b9118/"><FontAwesomeIcon icon={faLinkedin} /></a>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container-flush text-center">
+          <h1>some samples of my work</h1>
+          {work.map((item, key) =>
+              <div key={key} className="row">
+                <div className="col-md-6 p-4">
+                  <div className="image-holder">
+                    <img alt={item.col1.image.title} src={item.col1.image.src}></img>
+                  </div>
+                  <p className="text-left">{item.col1.copy}</p>
+                </div>
+                <div className="col-md-6 p-4">
+                  <div className="image-holder">
+                    <img alt={item.col2.image.title} src={item.col2.image.src}></img>
+                  </div>
+                  <p className="text-left">{item.col2.copy}</p>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </section>
+      <footer className="p-4">Thank you for taking the time to explore a sampling of my work. Let's build great apps together. <a href="mailto:kilian_sweeney@yahoo.com">Kilian Sweeney</a></footer>
+      </div>
   );
 }
 
---
+++ src/constants/constants.js
@@ -0,0 +1,117 @@
+import React from 'react';
+
+export const work = [
+    {
+      col1: {
+        image:{
+          title: 'Ontrac Event Management Dashboard',
+          src: require("../images/ontrac.png")
+        },
+        copy: <span>I coded the frontend for an event management dashboard application using angularjs, chartsjs, bootstrap and a java-based api. I designed the data visualizations and much of the layout. I also designed and coded the mobile version of this app using reactjs.</span>
+      },
+      col2: {
+        image:{
+          title: "Concentrate!",
+          src:require("../images/concentrate-2.jpg")
+        },
+        copy:<span>I designed and built a memory game (<a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/Concentrate/dp/B0083I6KBY/ref=sr_1_1?keywords=tigerBaby+Enterprises&qid=1569692378&s=gateway&sr=8-1">Concentrate!</a>) for the Amazon Kindle eReader market place in partnership with the Chicago scratch board artist Kathleen Judge. This game was coded with java and has been downloaded thousands of times.</span>
+      }
+    },
+    {
+      col1: {
+        image:{
+          title: 'Miami Zoo',
+          src: require("../images/miami-zoo.png"),
+        },
+        copy: <span>I was the lead front end and cms developer for a new <a target="_blank" rel="noopener noreferrer" href="https://www.zoomiami.org/">Miami Zoo website</a>. I used Drupal for the content management system. Along with developing the front end of the site itself and an intricate management system, I also trained the Miami Zoo staff on using the website. This one was a lot of fun!</span>
+      },
+      col2: {
+        image:{
+          title: 'Platinum Nights Scavenger Hunt game',
+          src: require("../images/justin-timberlake.jpg"),
+        },
+        copy: <span>I built the front-end code for a highly visible <a target="_blank" rel="noopener noreferrer" href="https://www.latfusa.com/article/2013/9/go-on-a-scavenger-hunt-with-justin-timberlake-bud/">scavenger hunt mobile-first web app</a> used by millions of scavenger hunt gamers. The game app was promoted by Justin Timberlake and Bud Light Platinum. I was tasked with choosing the framework for the app (I used foundation), building the html/css layer. I was also responsible for the considerable javascript logic. This is primarily a single page app with lots of logic to animate screens and load data via json.</span>
+      }
+    },
+    {
+      col1: {
+        image:{
+          title: 'Robot Maker Calculator',
+          src: require("../images/robot-maker-calculator.png"),
+        },
+        copy: <span>I originally built this engineer's calculator as a blackberry app for Kollmorgen. Later I converted the code using the Sencha Touch javascript library to make the Robot Maker Calculator. This is an engineer's calculator allowing for many various physics calculations, measurement conversions, etc. The Sencha Touch app is no longer online but <a target="_blank" rel="noopener noreferrer" href="https://bitbucket.org/tigerbaby/motioncalc/src/master/">the code</a> is and it's openely available to anybody who would like to host it. The Kollmorgen app can be found <a target="_blank" rel="noopener noreferrer" href="https://www.kollmorgen.com/en-us/service-and-support/technical/motioneering/motioneering/">here</a>.</span>
+      },
+      col2: {
+        image:{
+          title: 'Kollmorgen Distributor Locator',
+          src: require("../images/kollmorgen.png"),
+        },
+        copy: <span>I built a complex <a target="_blank" rel="noopener noreferrer" href="https://www.kollmorgen.com/en-us/where-to-buy/">distributor locator</a> for Kollmorgen utilzing the google maps api and the C# language and javascript. This app also ties into Salesforce for distributor data.</span>
+      }
+    },
+    {
+      col1: {
+        image:{
+          title: "Loyola University Chicago - What's My Major Quiz",
+          src: require("../images/luc-whats-my-major.png"),
+
+        },
+        copy: <span>Among the many enhancements I brought to the Loyola University website is the interactive <a target="_blank" rel="noopener noreferrer" alt="LUC.edu" href="https://www.luc.edu/undergrad/academiclife/whatsmymajorquiz/#">What's My Major quiz</a>. Other items I built or enhanced on luc.edu include responsive design features, background animations/videos, and intelligent collapsible menus.</span>
+        },
+      col2: {
+        image:{
+          title: '3TONS',
+          src: require("../images/3tons.png"),
+        },
+        copy: <span>Of course I designed and coded <a target="_blank" rel="noopener noreferrer" href="http://www.3tons.org/">my band's website</a>. It's a simple responsive site, designed to cleanly fit your device's screen size. That's me on the tele :-)</span>
+      }
+    },
+    {
+      col1: {
+        image:{
+          title: 'Tru View Facebook app',
+          src: require("../images/tru-view.png"),
+        },
+        copy: <span>I have built several facebook apps. This one is a <a target="_blank" rel="noopener noreferrer" href={require("../images/tru-view.png")}>Fans' Choice Award voting app</a> for Tru View, a picture frame company. The app collected voter choices and pulled voter data into Salesforce.</span>
+      },
+      col2: {
+        image:{
+          title: 'Redboard Hotel Wifi Dashboard App',
+          src: require("../images/redboard.png"),
+        },
+        copy: <span>Tying into live data from hotel wifi networks and a helpdesk system api, this app allows hotel managers to monitor bandwidth usage, user analytics, network health, helpdesk issues and more. Along with my wife I designed this dashboard app. I built it using Drupal, javascript and php.</span>
+      }
+    },
+    {
+      col1: {
+        image:{
+          title: 'Liftmaster Total Solution App',
+          src: require("../images/liftmaster-app.png")
+        },
+        copy: <span>Based on business requirements, I designed and built a touch device app for Liftmaster. This app is used at conferences and on sales calls to assist customers in determining the combination of products they should purchase based on their needs. The app was built with javascript, css3, and HTML5. It is a downloadable web app that can fully function offline. It was designed with iPads and large touchscreen monitors in mind.</span>
+      },
+      col2: {
+        image:{
+          title: 'Currency Converter',
+          src: require("../images/currency-converter.jpg")
+        },
+        copy: <span>I designed and built a <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/Currency-Converter/dp/B009Z0J07K/ref=sr_1_2?keywords=tigerBaby+Enterprises&qid=1569692262&s=gateway&sr=8-2">Currency Converter</a> for the Amazon Kindle eReader market place. This app was coded with java and has been downloaded thousands of times.</span>,
+      }
+    },
+    {
+      col1: {
+        image:{
+          title: 'Ontrac Sales Design Comp',
+          src: require("../images/ontrac-sales-site-bo.png")
+        },
+        copy: <span>I design as well as code. Here is <a target="_blank" rel="noopener noreferrer" href={require("../images/ontrac-sales-site.png")}>a comp</a> I created for a Wordpress-based responsive design website that I also ultimately built.</span>
+      },
+      col2: {
+        image:{
+          title: 'Mary Bange Pilates',
+          src: require("../images/mary-bange.png")
+        },
+        copy: <span>I designed and built this mobile friendly <a target="_blank" rel="noopener noreferrer" href="http://www.marybange.com/">Pilates studio website.</a> Mary Bange has been a client for many many years. I have been her main consultant for her web and social media presence.</span>
+      }
+    },
+  ];
